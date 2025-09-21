9c217972e070b840663b06cc560b5c5bec148fc5
momoirocloverz <1796583678@qq.com>
2025-09-21 12:55:05 +0800

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -176,7 +176,7 @@ function applyAndRender() {
     renderSources(['all', ...new Set(raw.map(x => x.source))]);
     // 彩蛋：输入 magic 试试看
     if (query === 'magic') {
-      alert('✨ 哇！你发现了隐藏功能！');
+      alert('仓库要重新fork');
     }
 }
 
